1f329057cb1d1d498be2acbdae8bba3bf6317a87
Mário César Silva <mcgato33@hotmail.com>
2022-05-02 15:00:03 -0100

    started

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+type ButtonProps = {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  return <button className="bg-[#8257e6] px-4 h-10 rounded text-violet-100 hover:bg-violet-700 transition-colors">{props.text ?? "Default"}</button>
+}
+
+function App() {
+
+  return (
+    <div className="flex gap-2">
+      <Button text="Enviar" />
+      <Button text="Ok" />
+      <Button />
+    </div>
+  )
+}
+
+export default App
